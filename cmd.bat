4113fa8886dffdc9f9a07734ecd98bae53a13cb0
daniel <daniel-yim@live.com>
2016-06-09 13:05:09 +0800

    初始化

--- cmd.bat
+++ cmd.bat
@@ -1,19 +1,29 @@
-:: ** Rockontrol Ant(rant) **
+:: ** TongDeploy Tools **
 
-:: 启动rant命令窗口，主要用来自动设置ant运行环境，并检测当前系统的java环境。
+::
 
+::
+:: version:2.0
+:: 2016-06-09 Daniel TongTech
 :: version:1.0
-:: 2015-01-10 daniel 创建
+:: 2015-01-10 Daniel Rockontrol
+:: 
 
 :: Author:daniel
 :: Email:daniel.yim@live.com
 
 @echo off
-::setting ant
-set ANT_HOME=%cd%
-set PATH=%ANT_HOME%\bin;%PATH%
-::监测java环境
-java -version
 
+@echo ----------------欢迎使用 TongDeploy Tools---------------
+
+:: setting Env
+set ANT_HOME=%cd%/lib/ant
+set M2_HOME=%cd%/lib/maven
+set PATH=%ANT_HOME%\bin;$M2_HOME/bin;%PATH%
+
+:: check Java 
+::java -version
+ant -version
+mvn -version
 @echo ---------------------Rockontrol Ant---------------------
 cmd.exe
