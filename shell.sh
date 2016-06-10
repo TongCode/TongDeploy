597100f307c6f0e44f3c21a1f76943a78f50995a
daniel <daniel-yim@live.com>
2016-06-10 17:40:33 +0800

    修复初始化环境后的终端方式

--- shell.sh
+++ shell.sh
@@ -14,15 +14,15 @@
 # Author:daniel
 # Email:daniel.yim@live.com
 
+echo 
 echo --------------欢迎使用 TongDeploy Tools---------------
+echo
 
 # setting Env
-
+export JAVA_HOME=$(pwd)/lib/jdk
 export ANT_HOME=$(pwd)/lib/ant
 export M2_HOME=$(pwd)/lib/maven
-#export JAVA_HOME=$(pwd)/lib/jdk
-export PATH=$ANT_HOME/bin:$M2_HOME/bin:$PATH
-#export PATH=$ANT_HOME/bin:$M2_HOME/bin:$JAVA_HOME/bin:$PATH
+export PATH=$ANT_HOME/bin:$M2_HOME/bin:$JAVA_HOME/bin:$PATH
 
 # init
 chmod 775 $ANT_HOME/bin/*
@@ -33,6 +33,11 @@ chmod 775 $M2_HOME/bin/*
 ant -version
 mvn --version
 
+echo
+echo 环境检测完成，请尽情享用..
 echo -------------------Tongtech Deploy-------------------
-/bin/sh
+echo
+echo
+
+/bin/bash
 
